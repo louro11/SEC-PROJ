8c6fa2a3af59e4323e17ff9f49185d2099de089d
Goncalo <goncalo_louro@hotmail.com>
2018-03-13 12:46:52 +0000

    add base files

---
+++ HDS-COIN/src/main/java/org/hds-coin/T26/ServerApp.java
@@ -0,0 +1,13 @@
+package org.hds-coin.T26;
+
+/**
+ * Hello world!
+ *
+ */
+public class ServerApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
